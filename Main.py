1eec76ddb4be7a4ca073f6181b422ec63f2a9cd6
yvonne <yvonnehuang0810@gmail.com>
2023-11-28 14:46:40 -0500

    changed one line

--- Main.py
+++ Main.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         input_text = ""
         input_label = tk.Label(
                 root,
-                text="test",
+                text="type something!",
                 font=("Arial", 40),
                 pady=10
         )
